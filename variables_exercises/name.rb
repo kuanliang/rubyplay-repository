62f5e509fd4968af6eb866c9c5e3d6d209c0e65d
kuanliang <kliangliu@gmail.com>
2015-04-01 18:22:29 +0800

    Varialbes Exercise

---
+++ variables_exercises/name.rb
@@ -0,0 +1,9 @@
+puts "Please enter your first name?"
+first_name = gets.chomp
+puts "Your last name please!!"
+last_name = gets.chomp
+puts "Hello Mr. #{first_name} #{last_name}!!"
+
+10.times do
+  puts first_name
+end
---
+++ variables_exercises/scope.rb
@@ -0,0 +1,14 @@
+a = 5
+arr = [1,2,3]
+
+3.times do
+  a =3
+  b =5  
+end
+
+for i in arr do
+  b = 6
+end
+
+puts a
+puts b
